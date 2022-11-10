af3a279ac8fc83523781f566451758075264833a
Sergei <greendaynul@gmail.com>
2022-11-10 21:20:23 +0300

    Написал программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿//Контрольная работа
 
+Console.Clear();
+Console.Write("Введите массив : ");
+int A = Convert.ToInt32(Console.ReadLine());
+int[,] Array = new int [i];
+
+for (int = 0; Array[i].Length(0); i++)
+if (Array[i].Length < 3);
+{
+    Massiv = i;
+}
+Console.WriteLine($"{Massiv}, ");
